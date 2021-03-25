183839ca4c60fff831c84169efb8f83777cdcacf
Anton <ssa.ssa.ssa11@gmail.com>
2021-03-25 16:50:40 +0200

    add GameController start endpoint

---
+++ src/main/java/com/anton/snl/controller/GameController.java
@@ -0,0 +1,29 @@
+package com.anton.snl.controller;
+
+import com.anton.snl.model.Game;
+import com.anton.snl.model.Token;
+import com.anton.snl.service.GameService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequestMapping("/game")
+public class GameController {
+    private final GameService gameService;
+
+    public GameController(GameService gameService) {
+        this.gameService = gameService;
+    }
+
+    @PostMapping("/start")
+    public ResponseEntity<Game> start(@RequestBody Token token) {
+        log.info("start game request: {}", token);
+
+        return ResponseEntity.ok(gameService.startGame(token));
+    }
+}
